a1255caba887169dbc1f08979c355dda56060429
Zishrodrigues <zish.rodrigues@gmail.com>
2017-02-09 15:23:37 +0100

    progress

--- opdracht 5 - spa/static/js/script.js
+++ opdracht 5 - spa/static/js/script.js
@@ -1,68 +1,37 @@
 (function() { // here an iffe is initiated as to keep the global scope empty and run the code on read
   "use strict"; // the strict line is used to make sure the function only runs in strict mode
 
+  window.location.hash = '#home'; //go to #home as soon as the page loads
+
   var app = { // the app object is declared
     init: function() { // the init function is declared and run
-      console.log('hi');
         routes.init(); //the routes function is called
     }
   };
 
   var routes = { // the route object is declared
     pages: [ //here an array is made with the page id's
-      '#home',
-      '#bestprac'
+      'home',
+      'bestprac'
     ],
     init: function() { // the routes.init function is declared
-      console.log('hi from routes');
       window.addEventListener('hashchange', sections.toggle, false);
     }
   };
 
-  var sections = { // the sections object is declared
-    toggle: function() { // create toggle function which will check the hash toggle
-      var route = window.location.hash; // declare variable in which route is equal to the location hash
-      if (route === '#home') {
-        console.log('test');
-      } else {
-        console.log('lel');
+  var sections = {
+      // loop for each get element by id
+      toggle: function(){
+          var route = window.location.hash;
+          routes.pages.forEach(function (page){
+            if ('#' + page  === route ) {
+                document.getElementById(page).classList.remove('hidden');
+            } else {
+                document.getElementById(page).classList.add('hidden');
+            }
+          });
       }
-    }
   };
 
   app.init(); //the main app function is called
 })();
-
-
-
-
-/*
-
-(function() {
-    "use strict";
-    var person = {
-        name: 'jane doe',
-        speak: function() {
-            console.log('hello, my name is ' + this.name);
-        }
-    };
-
-    console.log(person);
-    person.speak();
-})();
-
-(function() {
-    "use strict";
-    var car = {
-        color: 'green',
-        year: '1991',
-        brand: 'tesla',
-        describe: function() {
-            console.log('This car is ' + this.color + ', from the year ' + this.year + ' and made by ' + this.brand);
-        }
-    };
-
-    console.log(car);
-    car.describe();
-})();
-*/
