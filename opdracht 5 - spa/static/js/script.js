5b41d893066709b6b44e76caad5c3a86775868cb
Zishrodrigues <zish.rodrigues@gmail.com>
2017-02-09 13:58:32 +0100

    js progress

--- opdracht 5 - spa/static/js/script.js
+++ opdracht 5 - spa/static/js/script.js
@@ -1,28 +1,42 @@
-(function() {
-    "use strict";
+(function() { // here an iffe is initiated as to keep the global scope empty and run the code on read
+  "use strict"; // the strict line is used to make sure the function only runs in strict mode
 
-    var app = {
-        init: function() {
-            routes.init();
-        }
-    };
+  var app = { // the app object is declared
+    init: function() { // the init function is declared and run
+      console.log('hi');
+        routes.init(); //the routes function is called
+    }
+  };
 
-    var routes = {
-        init: function() {
-            //console.log('test');
-        }
-    };
+  var routes = { // the route object is declared
+    pages: [ //here an array is made with the page id's
+      '#home',
+      '#bestprac'
+    ],
+    init: function() { // the routes.init function is declared
+      console.log('hi from routes');
+      window.addEventListener('hashchange', sections.toggle, false);
+    }
+  };
 
-    var sections = {
-    };
+  var sections = { // the sections object is declared
+    toggle: function() { // create toggle function which will check the hash toggle
+      var route = window.location.hash; // declare variable in which route is equal to the location hash
+      if (route === '#home') {
+        console.log('test');
+      } else {
+        console.log('lel');
+      }
+    }
+  };
 
-    app.init();
+  app.init(); //the main app function is called
 })();
 
 
 
 
-// EXAMPLE
+/*
 
 (function() {
     "use strict";
@@ -51,3 +65,4 @@
     console.log(car);
     car.describe();
 })();
+*/
