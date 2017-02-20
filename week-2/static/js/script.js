d51833df34ff55ef7493b8e606a1dda971339018
Zishrodrigues <zish.rodrigues@gmail.com>
2017-02-20 15:19:37 +0100

    added more comments into js

--- week-2/static/js/script.js
+++ week-2/static/js/script.js
@@ -6,22 +6,22 @@
   var app = { // the app object is declared
     init: function() { // the init function is declared and run
         routes.init(); //the routes function is called
-        getData.init();
+        getData.init(); //the getData function is called
     }
   };
 
   var routes = { // the route object is declared
     init: function(data) { // the routes.init function is declared
       routie({
-          'list': function() {
-            document.querySelectorAll('.home').forEach(function (homeClass) {
-              homeClass.classList.remove('hidden');
+          'list': function() { //function that's activitated when url = #list
+            document.querySelectorAll('.home').forEach(function (homeClass) { // Loop through all elements with class .home
+              homeClass.classList.remove('hidden'); // Go through the classes with .hidden and remove the class from element
             });
             document.querySelectorAll('.card-detail').forEach(function (cardDetail) {
               cardDetail.classList.add('hidden');
             });
           },
-          'list/:cardId': function(cardId) {
+          'list/:cardId': function(cardId) { //Function that's activitated when url = #list/cardId
             document.getElementById(cardId).classList.remove('hidden');
             document.querySelectorAll('.home').forEach(function (homeClass) {
               homeClass.classList.add('hidden');
@@ -31,24 +31,20 @@
     }
   };
 
-  //set variables in library object
-  var library = {
-    main : document.querySelector('main'),
-    template : document.querySelector('#template'),
-    source : template.innerHTML,
-    urlPromoCards: 'https://omgvamp-hearthstone-v1.p.mashape.com/cards/sets/Promo',
+  var config = { //set variables in config object
+    main : document.querySelector('main'), // Select element main
+    template : document.querySelector('#template'), //select element with id #template
+    source : template.innerHTML, //select the HTML of #template to change its content
+    urlPromoCards: 'https://omgvamp-hearthstone-v1.p.mashape.com/cards/sets/Promo', //api link
     html: ''
   };
 
-  // render the data to the html here
-  var renderData = {
-    render: function(data) {
-      data.forEach(function(item, i) {
-        var compile = Handlebars.compile(library.source);
-        library.html = compile(item);
-        library.main.innerHTML += library.html;
-        var cardMatcher = item.cardId;
-        console.log(cardMatcher);
+  var renderData = { // render the data to the html here
+    render: function(data) { // use the api data for this function
+      data.forEach(function(item) { //Loop through each data object and put it in item
+        var compile = Handlebars.compile(config.source); //compile the data into #template
+        config.html = compile(item); //compile an item into the html
+        config.main.innerHTML += config.html; //add the HTML into the main element
       });
     }
   };
@@ -57,7 +53,7 @@
   var getData = {
     init: function() {
       aja()
-        .url(library.urlPromoCards)
+        .url(config.urlPromoCards)
         .header('X-Mashape-Key', '2sTOhVU46SmshEg17iL8fyLAEp9Hp1B5PGBjsnsJ2tUf1zkppp')
         .on('200', function(data){
             //data is a javascript object
