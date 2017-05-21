c520f584588bdc88a0e2ef2af37482b40b8e7e05
Zishrodrigues <zish.rodrigues@gmail.com>
2017-05-21 11:35:58 +0200

    added final-her

--- final-her/README.md
+++ final-her/README.md
@@ -1,13 +1,1 @@
-# Web app Final: API and templating #
-#Web app link:#
-https://oege.ie.hva.nl/~khushiz001/webapp
-
-##Features##
-* See Hearthstone promo cards
-* Get information regarding sets, stats en flavor text
-
-##Flow##
-![alt tag](https://scontent.xx.fbcdn.net/v/t35.0-12/17093050_593353147524598_980627492_o.jpg?oh=7725108dafd802c8483eee8c4fe113e0&oe=58BB8D6B)
-
-##Sources##
-Unnofficial Hearthstone API: https://market.mashape.com/omgvamp/hearthstone
+#projectweek-een
--- final-her/static/js/script.js
+++ final-her/static/js/script.js
@@ -1,22 +1,13 @@
 (function() { // here an iffe is initiated as to keep the global scope empty and run the code on load
     "use strict"; // the strict line is used to make sure the functions only runs in strict mode
 
-    window.location.hash = '#list'; //go to #list as soon as the page loads
-
-    var config = { //set variables to be used throughout the code
-        setList: {
-            main : document.querySelector('section'), // This is the element that holds the html
-            template: document.querySelector('list-template'), //This is the element that holds the template html to be added in main
-            source: template.innerHTML, //This selects the template and used innerHTML to inject new html into the element
-            url: 'https://omgvamp-hearthstone-v1.p.mashape.com/info/', //API url for general info
-        },
-        general: {
-            main : document.querySelector('main'), // This is the element that holds the html
-            template: document.querySelector('template'), //This is the element that holds the template html to be added in main
-            source: template.innerHTML, //This selects the template and used innerHTML to inject new html into the element
-            url: 'https://omgvamp-hearthstone-v1.p.mashape.com/cards/sets/', //API url
-            html: '' //empty html which will hold the new html
-        }
+    var config = {
+        url: apiUrl,
+        radioButton: document.querySelector('input'),
+        main : document.querySelector('main'), // This is the element that holds the html
+        template: document.querySelector('template'), //This is the element that holds the template html to be added in main
+        source: template.innerHTML, //This selects the template and used innerHTML to inject new html into the element
+        html: '' //empty html which will hold the new html
     };
 
     var app = {
@@ -28,30 +19,110 @@
     var routes = {
         init: function(data) {
             routie({
-                'list': function() { //function that's activitated when url = #list
-                    dataWorker.getData(config.setList.url);
+                '': function() { //function that's activitated when url = #list
+                    window.location.hash = 'intro';
                 },
-                'list/:cardId': function() { //Function that's activitated when url = #list/cardId
+                'intro': function() { //function that's activitated when url = #list
+                    document.querySelector('.intro').classList.remove('hide');
+                },
+                'placeForm': function() { //function that's activitated when url = #list
+                    document.querySelector('.intro').classList.add('hide');
+                    document.getElementById('placeForm').classList.remove('hide');
+                    filters.formSubmit('placeForm', 'place', 'roomForm');
+                },
+                'roomform': function() { //function that's activitated when url = #list
+                    filters.formSubmit('roomForm', 'rooms', 'spaceForm');
+                },
+                'spaceForm': function() { //function that's activitated when url = #list
+                    filters.formSubmit('spaceForm', 'space', 'result');
+                },
+                'result':function() {
+                    dataWorker.getData(config.url);
+                    // document.getElementById('placeForm').classList.add('hide');
+                    document.querySelectorAll('.overview').forEach(function (overview) { // Loop through all elements with class .home
+                      overview.classList.remove('hide'); // Go through the classes with .hidden and remove the class from element
+                    });
+                    document.querySelectorAll('.detail').forEach(function (detail) {
+                      detail.classList.add('hide');
+                    });
+                },
+                'reset':function() {
+                    location.reload();
+                    window.location.hash = 'intro';
+                },
+                'result/:Id': function(Id) {
+                    console.log(Id);
+                    document.getElementById(Id).classList.remove('hide');
+                    document.querySelectorAll('.overview').forEach(function (overview) {
+                      overview.classList.add('hide');
+                    });
                 }
             });
         }
     };
 
+    var filters = {
+        getRadioVal: function(form, name) {
+            var val;
+            // get list of radio buttons with specified name
+            var radios = form.elements[name];
+            // loop through list of radio buttons
+            for (var i=0, len=radios.length; i<len; i++) {
+                if ( radios[i].checked ) { // radio checked?
+                    val = radios[i].value; // if so, hold its value in val
+                    break; // and break out of for loop
+                }
+            }
+            return val; // return value of checked radio or undefined if none checked
+        },
+        parameters: [],
+        formSubmit: function(formId, name, formLink) {
+            document.getElementById(formId).onsubmit = function() {
+                var val = filters.getRadioVal(this, name);
+                filters.parameters.push('/' + val);
+                window.location.hash = '#' + formLink;
+                filters.showHide(formLink, formId);
+                console.log(filters.parameters);
+                return false;
+            };
+        },
+        showHide: function(show, hide) {
+            document.getElementById(show).classList.remove('hide');
+            document.getElementById(hide).classList.add('hide');
+        }
+    };
+
     var dataWorker = {
+        dataHolder: [],
         getData: function(url) {
             aja()
-              .url(url)
-              .header('X-Mashape-Key', '2sTOhVU46SmshEg17iL8fyLAEp9Hp1B5PGBjsnsJ2tUf1zkppp')
-              .on('200', function(data){
-                  //data is a javascript object
-                  dataWorker.render(data.sets);
+                .url(url + filters.parameters.join(''))
+                .on('200', function(data){
+                //data is a javascript object
+                    dataWorker.render(data.Objects);
+                    dataWorker.filterBrochure(data.Objects);
+                    data.Objects.map(function(item) {
+                        dataWorker.dataHolder.push(item);
+                    });
+                    localStorage.setItem("dataHolder", JSON.stringify(dataWorker.dataHolder));
+                    console.log(dataWorker.dataHolder);
                 })
             .go();
         },
         render: function(data) {
+            config.main.innerHTML = '';
             data.forEach(function(item) {
-                console.log(item);
+                var compile = Handlebars.compile(config.source);
+                config.html = compile(item);
+                config.main.innerHTML += config.html;
             });
+            console.log(JSON.parse(localStorage.getItem("dataHolder")));
+        },
+        filterBrochure: function(data) {
+            var filtered = data.filter(function(val) {
+                return val.HeeftBrochure === false;
+            });
+            // dataWorker.render(filtered);
         }
     };
 
